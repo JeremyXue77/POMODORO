f2bda6121bb43a500ec3b36f9897568beeecef7c
Jeremy Xue <gg831006@gmail.com>
2020-04-06 09:46:29 +0800

    🚧 (TimerView): Add TimerView and set up basic IBOutlet

---
+++ Pomodoro/View/Timer/TimerView.swift
@@ -0,0 +1,17 @@
+//
+//  TimerView.swift
+//  Pomodoro
+//
+//  Created by Jeremy Xue on 2020/4/6.
+//  Copyright © 2020 jeremyxue. All rights reserved.
+//
+
+import UIKit
+
+class TimerView: UIView {
+
+    // MARK: IBOutlet
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+}
