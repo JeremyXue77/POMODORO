087d5e29e246c20b1a7cf70320b93bc0ff857afb
Jeremy Xue <gg831006@gmail.com>
2020-04-06 10:58:38 +0800

    🚧 (UITest, UnitTest): Remove UITest and add UnitTest

---
+++ PomodoroTests/PomodoroTests.swift
@@ -0,0 +1,33 @@
+//
+//  PomodoroTests.swift
+//  PomodoroTests
+//
+//  Created by Jeremy Xue on 2020/4/6.
+//  Copyright © 2020 jeremyxue. All rights reserved.
+//
+
+import XCTest
+
+class PomodoroTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
